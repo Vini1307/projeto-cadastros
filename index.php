1449c16edf3f82612fc417b9495ec1bcc6a97c83
Michel <michel@flexasealbr.local>
2024-03-21 15:15:29 -0300

    commit frontend

--- index.php
+++ index.php
@@ -8,19 +8,50 @@
         crossorigin="anonymous"/>
         <link rel="stylesheet" href="CSS/style.css">
 
+        <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap4.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+      integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+      crossorigin="anonymous"
+    />
+    <script
+      src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+      integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+      integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+      integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+      crossorigin="anonymous"
+    ></script>
+
         <title>CADASTRO DOS CLIENTES</title>
 
     </head>
 
     <body>
         <div class="container">
+            <div class="login">
             <div class="row">
                 <div class="col">
                     <h2>Cadastro clientes</h2>
                 </div>
             </div>
             <div class="row">
-                <div class="col -4">
+                <!-- <div class="col -4"> -->
                     
                 </div>
                 <div class="col -8">
@@ -39,17 +70,18 @@
                         </div>
                         <div class="mt-3 form-floating">
                             <input type="text" class="form-control" id="cnpj" name="cnpj" >
-                            <label for="cnpj" class="form-label">cnpj</label>
+                            <label for="cnpj" class="form-label">Cnpj</label>
                         </div>
                         <div class="mt-3 form-floating">
                             <div class="row">
-                             <div class="col"><button type="button" class="btn btn-primary form-control botaonovo">Novo</div>
+                             <!-- <div class="col"><button type="button" class="btn btn-primary form-control botaonovo">Novo</div> -->
                                 <div class="col"><button type="submit" class="btn btn-primary form-control botaosalvar">Salvar</div>
                             </div>
                         </div>
                     </form>
                 </div>
             </div>
+        </div> 
         </div>
 
 
